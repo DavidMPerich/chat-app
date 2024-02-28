334c3dc05d9e64d2b5441a13d9e9f9ea333b0b22
DavidMPerich <davidmperich@gmail.com>
2024-02-28 06:47:01 -0700

    add multiple user functionality

--- client.js
+++ client.js
@@ -13,6 +13,10 @@ const socket = net.createConnection({ host: "127.0.0.1", port: 3008 }, async () 
     socket.write(message);
 });
 
+socket.on("data", (data) => {
+    console.log(data.toString("utf-8"));
+});
+
 socket.on("end", () => {
     console.log("Connection was ended!");
 });
